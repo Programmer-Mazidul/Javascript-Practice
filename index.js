2e7f29fe79dd1827ef7444bb8edb71819b275e92
Mazid <freelancer.mazid@gmail.com>
2020-04-30 11:41:46 +0600

    variable

---
+++ index.js
@@ -0,0 +1,21 @@
+var applePrice = 12;
+console.log(typeof applePrice);
+
+var phoneNumber = 01400340753;
+console.log(typeof phoneNumber);
+
+var name = "Mazidul Haque";
+console.log(typeof name);
+
+var animal = "Camel";
+console.log(typeof animal);
+
+var isHot = true;
+console.log(typeof true);
+
+var isCool = false;
+console.log(typeof false);
+
+
+var ipAddres = 12.887;
+console.log(typeof ipAddres);
